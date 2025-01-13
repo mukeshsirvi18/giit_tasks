ad2b685742dc88980aa052c549ce16bf617fff06
nandusathyan <nandusathyan0@gmail.com>
2025-01-13 11:30:36 +0530

    eks-comput service

--- eks/eks.tf
+++ eks/eks.tf
@@ -13,11 +13,23 @@ module "eks" {
 
   cluster_compute_config = {
     enabled    = true
-    node_pools = ["general-purpose"]
+    node_pools = ["general-purpose"]     //AWS defaults to EC2-based compute
   }
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    example = {
+      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+      ami_type       = "AL2023_x86_64_STANDARD"
+      instance_types = ["t3.medium"]
+
+      min_size     = 1
+      max_size     = 1
+      desired_size = 1
+    }
+  }
 
   tags = {
     Env = "dev"
