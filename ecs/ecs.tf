007a429e1aa7574de6702ddb8a070b96b40cab33
nandusathyan <nandusathyan0@gmail.com>
2025-01-13 16:33:26 +0530

    ecs

---
+++ ecs/ecs.tf
@@ -0,0 +1,38 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+//https://registry.terraform.io/modules/terraform-aws-modules/ecs/aws/latest/submodules/cluster
+
+module "ecs_cluster" {
+  source = "terraform-aws-modules/ecs/aws//modules/cluster"
+
+  cluster_name = "ecs-fargate"
+
+  cluster_configuration = {
+    execute_command_configuration = {
+      logging = "OVERRIDE"
+      log_configuration = {
+        cloud_watch_log_group_name = "/aws/ecs/aws-ec2"
+      }
+    }
+  }
+
+  fargate_capacity_providers = {
+    FARGATE = {
+      default_capacity_provider_strategy = {
+        weight = 50
+      }
+    }
+    FARGATE_SPOT = {
+      default_capacity_provider_strategy = {
+        weight = 50
+      }
+    }
+  }
+
+  tags = {
+    Environment = "Development"
+    Project     = "EcsEc2"
+  }
+}
